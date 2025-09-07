9ff0893146965998ad3e6935f511115890ec6a9d
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2025-09-07 09:38:04 +0530

    checking something

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,7 +5,7 @@ import { postState } from './state/postAtom';
 function PostItem({ id }) {
   const [posts, setPosts] = useRecoilState(postState);
 
-  const post = posts.find(p => p.id === id);
+  const post = posts.find(p => p.id === id); //finding post using Id.
 
   function handleLike() {
     setPosts(posts.map(p =>
